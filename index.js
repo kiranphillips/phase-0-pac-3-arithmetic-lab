bc648e926d98b25939d4d73db584998b7c63c5f0
Kiran Phillips <kiranphillips61@gmail.com>
2022-03-29 12:11:44 +0200

    commit tree 829d8e69c28381986cbcc63cd2399935697b2640

---
+++ index.js
@@ -0,0 +1,35 @@
+function add() {
+  }
+  function subtract() {
+}
+function multiply() {
+}
+function divide() {
+}
+function add(a, b) {
+    return a + b;
+  }
+  function subtract(a, b) {
+    return a - b;
+  }
+  function multiply(a, b) {
+    return a * b;
+  }
+  function divide(a, b) {
+    return a / b;
+  }
+  function increment(n) {
+    return (n += 1)
+}
+function decrement(n) {
+    return (n -= 1)
+}
+function makeInt(n) {
+   return parseInt(n, 10)
+}
+makeInt();
+
+function preserveDecimal(n) {
+   return parseFloat(n);
+}
+console.log(preserveDecimal(2.222));
